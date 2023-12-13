ca9a15d601a1d4dbc2962729b84d8e31de1bd520
Amir Ghadanfar <ghadanfar@cl.uni-heidelberg.de>
2023-12-13 19:06:57 +0100

    add Gesture Detector and create an actual target page with recipe information

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -26,7 +27,7 @@ class RecipeApp extends StatelessWidget {
         ),
       ),
       // 6
-      home: const MyHomePage(title: 'PC Calculator'),
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
 }
@@ -60,35 +61,67 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       // 3
       body: SafeArea(
-      // 4
+        // 4
         child: ListView.builder(
           // 5
           itemCount: Recipe.samples.length,
           // 6
           itemBuilder: (BuildContext context, int index) {
             // 7
-            // TODO: Add GestureDetector
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+
           },
         ),
       ),
     );
   }
-
-  Widget buildRecipeCard(Recipe recipe) {
-    // 1
-    return Card(
-      // 2
-      child: Column(
-        // 3
-        children: <Widget>[
-          // 4
-          Image(image: AssetImage(recipe.imageUrl)),
-          // 5
-          Text(recipe.label),
-        ],
-      ),
-    );
-  }
 }
 
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            // 5
+            const SizedBox(
+              height: 14.0,
+            ),
+            // 6
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  } // Flutter inspector is a powerful tool to watch over your widgets
